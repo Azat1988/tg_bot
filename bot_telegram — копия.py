da1aecd4ca403f7e9c840b4e49e2cfa358c98054
AzatN <nar88@ya.ru>
2022-08-12 14:19:29 +0500

    update 1208202

--- bot_telegram — копия.py
+++ bot_telegram — копия.py
@@ -4,8 +4,8 @@ from aiogram.utils import executor
 
 import os
 
-bot = Bot(token=os.getenv('TOKEN'), proxy=os.getenv('HTTP_PROXY_TG'))
-#bot = Bot(token='5562475997:AAE9ID-AKZDbbBMkQ9YYCGSvoIruwTXzMnA', proxy='http://10.22.1.135:3128')
+#bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token='5562475997:AAE9ID-AKZDbbBMkQ9YYCGSvoIruwTXzMnA')
 dp = Dispatcher(bot)
 
 @dp.message_handler()
--- strart_bot_tg.bat
+++ strart_bot_tg.bat
@@ -1,10 +1,13 @@
 @echo off
 
 call %~dp0venv\Scripts\activate
-
+echo "call %~dp0venv\Scripts\activate"
 
 set TOKEN=5562475997:AAE9ID-AKZDbbBMkQ9YYCGSvoIruwTXzMnA
+set HTTP_PROXY_TG=http://10.22.1.135:3128
 
 python %~dp0bot_telegram.py
+echo "python %~dp0bot_telegram.py"
+
 
 pause
